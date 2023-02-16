30c86dc363e55fb7faa1dfdd64407b8c7c9f84b3
Nashy-97 <normanshumba.ns@gmail.com>
2023-02-16 13:28:09 -0800

    Size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- * main - Program tha prints the size of various types on the computer it is compile and run on
- * Return 0 (Success)
+ * main - Program that prints the size of various types on the computer.
+ * Return: 0 (Success)
  */
+
 int main(void)
 {
 	char a;
