3039ee0ac9c7215378738b7f591a0f1ba39256ac
Nashy-97 <normanshumba.ns@gmail.com>
2023-02-21 13:59:47 -0800

    last digitclear

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * print_last_digit - prints the last digit of a number
+ * print_last_digit - prints the last digit
  * @n: the number to be treated
- * Return: value of the last digit of a number
+ * Return: the last digit.
  */
 int print_last_digit(int n)
 {
